4d94fcbe54193cc90a3ca4aaeec5ffa7aaa661ff
Mohamed Amin Kedir <m.aminkedir@gmail.com>
2021-02-04 12:16:13 -0800

    last nights code finish

--- src/MultiSelect/MultiSelect.js
+++ src/MultiSelect/MultiSelect.js
@@ -1,4 +1,4 @@
-import React, {} from 'react'
+import React, { useState } from 'react'
 import './MultiSelect.scss'
 
 // this is a stub for you to develop the following
@@ -16,11 +16,60 @@ import './MultiSelect.scss'
 */
 
 const MultiSelect = props => {
+    const { questionText, options, feedback } = props.data;
+    const [feedbackOutput, setFeedbackOutput] = useState([]);
+
+
+    const handleSubmit = (e) => {
+        const selected = [];
+        const elements = e.target.elements;
+        console.log('this is elements', elements)
+        for (let i = 0; i < elements.length; i++) {
+            selected.push(elements[i].checked);
+        }
+
+        setFeedbackOutput(options.map((option, index) => {
+            if (option.correct && selected[index] || (!option.correct && !selected[index])) {
+                return 'correct'
+            } else {
+                return 'incorrect'
+            }
+        }));
+    }
     return (
-        <div className={`MultiSelect`}>
-            multi-select implementation goes here
-        </div>
+        <form className={`MultiSelect`} onSubmit={e => {
+            e.preventDefault();
+            handleSubmit(e);
+        }}>
+            <h1 className="multiSelect-question">
+                {questionText}
+            </h1>
+            <div className="options">
+                {options?.map((option, index) => {
+                    return (
+                        <div className="option-text" key={index}>
+                            <input id={`multi-select-${index}`} name={`multi-select-${index}`} type="checkbox" className="option" />
+                            <label htmlFor={`multi-select-${index}`}> {option.text} </label>
+                            {(feedbackOutput[index] === 'incorrect') &&
+                                <div className="feedback-incorrect">
+                                    <div className="feedbackHeader">{feedback.incorrect.header}</div>
+                                    <div className="feedbackBody">{feedback.incorrect.body}</div>
+                                </div>
+                            }
+                            {(feedbackOutput[index] === 'correct') &&
+                                <div className="feedback-correct">
+                                    <div className="feedbackHeader">{feedback.correct.header}</div>
+                                    <div className="feedbackBody">{feedback.correct.body}</div>
+                                </div>
+                            }
+                        </div>
+                    );
+                })}
+            </div>
+
+            <button type="submit"> Submit </button>
+        </form>
     )
 }
 
-export default MultiSelect
+export default MultiSelect;
--- src/SingleSelect/SingleSelect.js
+++ src/SingleSelect/SingleSelect.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import './SingleSelect.scss'
 
 const SingleSelect = props => {
@@ -17,36 +17,50 @@ const SingleSelect = props => {
     const [selected, setSelected] = useState(-1);
 
     const handleSelect = (i) => {
-        if(selected === -1) setSelected(i)
+        if (selected === -1) setSelected(i)
     }
 
     const selectedOption = props.data.options[selected]
 
     return (
         <div className={`SingleSelect`}>
-            <h1>
-                {props.data.questionText}
-            </h1>
-            {
-                selected === -1 &&
-                props.data.options.map((option, optionIndex) => {
-                    return <button onClick={()=>{handleSelect(optionIndex)}}>{option.text}</button>
-                })
-            }
+            <div className="SingleSelect-Question">
+                <h1>
+                    {props.data.questionText}
+                </h1>
+            </div>
+            <div className="answer-key-grouping">
+                <div className="letterKeyOptions">
+                    {
+                        selected === -1 &&
+                        props.data.options.map((option, optionIndex) => {
+                            return <h className="letter-button" onClick={() => { handleSelect(optionIndex) }}>{option.letter}</h>
+                        })
+                    }
+                </div>
+                <div className="answerOptions">
+                    {
+                        selected === -1 &&
+                        props.data.options.map((option, optionIndex) => {
+                            return <button className="answer-button btn" onClick={() => { handleSelect(optionIndex) }}>{option.text}</button>
+                        })
+                    }
+                </div>
+            </div>
             {
                 selected > -1 &&
                 <div className={`feedback ${selectedOption.correct ? 'correct' : 'incorrect'}`}>
                     <h1>
                         {selectedOption.correct ?
                             props.data.feedback.correct.header
-                        :
+                            :
                             props.data.feedback.incorrect.header
                         }
                     </h1>
                     <p>
                         {selectedOption.correct ?
                             props.data.feedback.correct.body
-                        :
+                            :
                             props.data.feedback.incorrect.body
                         }
                     </p>
